3fbbe88bc5b2120a4cbe30eb1623d735db7a3bc2
Sam Guerrero <samkguerrero@gmail.com>
2019-04-09 14:40:06 -0700

    inital commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using crudelicious.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace crudelicious.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MyContext dbContext;
+        public HomeController(MyContext context)
+        {
+            dbContext = context;
+        }
+
+        [Route("")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Dish> AllDishesUnsorted = dbContext.Dishes.ToList();
+            List<Dish> AllDishes = AllDishesUnsorted.OrderByDescending(d => d.CreatedAt).ToList();
+            return View(AllDishes);
+        }
+
+        [Route("new")]
+        [HttpGet]
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        [Route("new")]
+        [HttpPost]
+        public IActionResult New(Dish newDish)
+        {
+            // System.Console.WriteLine("The new dish: ");
+            // System.Console.WriteLine(newDish.Name);
+            // System.Console.WriteLine(newDish.Chef);
+            // System.Console.WriteLine(newDish.Tastiness);
+            // System.Console.WriteLine(newDish.Calories);
+            // System.Console.WriteLine(newDish.Description);
+            if(ModelState.IsValid) 
+            {
+                dbContext.Add(newDish);
+                dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else 
+            {
+                return View("New");
+            }
+        }
+
+        [Route("edit/{dishid}")]
+        [HttpGet]
+        public IActionResult Edit(int dishid)
+        {
+            Dish showDish = dbContext.Dishes.FirstOrDefault(dish => dish.DishId == dishid);
+            return View(showDish);
+        }
+
+        [Route("edit/{dishid}")]
+        [HttpPost]
+        public IActionResult Edit(int dishid, Dish editDishNew)
+        {
+            if(ModelState.IsValid) 
+            {
+                Dish editDishBefore = dbContext.Dishes.FirstOrDefault(dish => dish.DishId == dishid);
+                editDishBefore.Name =  editDishNew.Name;
+                editDishBefore.Chef =  editDishNew.Chef;
+                editDishBefore.Tastiness =  editDishNew.Tastiness;
+                editDishBefore.Calories =  editDishNew.Calories;
+                editDishBefore.Description =  editDishNew.Description;
+                dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else 
+            {
+                return View("edit/dishid");
+            }
+        }
+
+        [Route("delete/{dishid}")]
+        [HttpGet]
+        public IActionResult Delete(int dishid)
+        {
+            Dish dishToDelete = dbContext.Dishes.SingleOrDefault(d => d.DishId == dishid);
+            dbContext.Dishes.Remove(dishToDelete);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [Route("{dishid}")]
+        [HttpGet]
+        public IActionResult Show(int dishid)
+        {
+            Dish showDish = dbContext.Dishes.FirstOrDefault(dish => dish.DishId == dishid);
+            return View(showDish);
+        }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using crudelicious.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace crudelicious
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string mySqlConnection = "server=localhost;userid=root;password=root;port=3306;database=dishes;SslMode=None";
+            services.AddDbContext<MyContext>(options => options.UseMySql(mySqlConnection));
+            services.AddSession();            
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
